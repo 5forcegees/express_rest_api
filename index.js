4da7d331d700c43f543c3c635c660f413ec0d5b2
glenn.dufresne1 <glenn.dufresne@accenture.com>
2016-01-28 10:24:55 -0800

    got the node server pulling the data from the db right.  but not returning the data yet

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var pkg = require(__dirname + '/package.json');
+var Sequelize = require('sequelize');
+
+var seq = new Sequelize(pkg.database_name, pkg.database_user, pkg.database_pw, {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+// load models, allows for adding models later if needed, just add them in app/models and then to this array
+var models = [
+    'whole_sheet'
+];
+
+models.forEach(function(model) {
+    module.exports[model] = seq.import(__dirname + '/app/models/' + model);
+});
+
+// export connection
+module.exports.seq = seq;
+
+
 app.set('models', require(__dirname + '/app/models/api.js'));
 
 //seq.({
@@ -25,35 +46,54 @@ router.get('/', function(req, res) {
     var field_name = req.param('field_name');
     var field_value = req.param('field_value').split(',');
     var sankey = req.param('sankey');
-    var ret = [];
     var nodes = [];
+    var ret_nodes = [];
     var links = [];
 
+    console.log(sankey);
     if (sankey){
         field_value.forEach(function(val){
-            whole_sheet.find({ where: {screen: val }}).then(function(matched_row){
-                var screen = matched_row.screen;
-                var facade = matched_row.facade_api;
-                var core = matched_row.core_api;
-                var e = matched_row.e_wsil;
-                console.log(screen+' screen '+facade+' facade '+core+' core '+ e + ' e ');
-                //push each layer into nodes array if not present
-
-                //get the array index of each value and push onto links array
-
-
-                console.log(matched_row.get());
-
-                ret.push(matched_row.get());
+            seq.query('select screen_name, facade_api, core_api, e_wsil from whole_sheet where screen = ? ',
+                { replacements: [val] }).then(function(resultsets) {
+                resultsets.forEach(function(results) {
+                    //console.log(results);
+                    results.forEach(function(result){
+                        var previous_key = '';
+                        for (var key in result) {
+                            if (result.hasOwnProperty(key)) {
+                                //console.log(key + " -> " + result[key]);
+                                if (nodes.indexOf(result[key]) == -1 ){
+                                    //using the nodes array to easily detect nodes I've already pushed
+                                    nodes.push(result[key]);
+                                    //returning the array of json objects
+                                    ret_nodes.push({name: result[key], id: result[key].toString().replace(' ', '_')});
+                                    //console.log("pushed the "+ result[key]+ " onto return nodes");
+                                }
+                            }
+                            if (previous_key.length > 0 ){
+                                //TODO: count the number of times each link is made to set the 'value' in the links obj
+                                links.push({source: nodes.indexOf(previous_key), target: nodes.indexOf(key), value: 1});
+                            }
+                            previous_key = key;
+                        }
+                    });
+                });
+                console.log("in the seq.query links.count = " + links.count);
+                console.log("and links = " + links);
             });
-
-        //assemble the links and array values into ret array
-
+            console.log("just after the seq.query links.count = "+links.count);
+            console.log("and links = " + links);
         });
-    }
+        var returnme = {nodes: ret_nodes, links: links};
+        res.json(returnme);
 
-    res.json({
-        message: 'hooray! welcome to our api!  Here is the request field_name: ' + field_name + ', field_value: ' + field_value[0] + ' + ' + field_value[1] +', sankey: ' + sankey + " ret: " + ret });
+        console.log("just after the field_value.foreach links.count = "+links.count);
+        console.log("and links = " + links);
+    }else{
+        console.log("into else");
+        var returnme = {nodes: ret_nodes, links: links};
+        res.json(returnme);
+    }
 });
 
 // more routes for our API will happen here
